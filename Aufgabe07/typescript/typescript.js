710400e5f7b99028ea8298bc50671ba6dedca999
MonaKabelka <mona.kabelka.mk@gmail.com>
2020-12-01 16:46:33 +0100

    scheiße :)

--- Aufgabe07/typescript/typescript.js
+++ Aufgabe07/typescript/typescript.js
@@ -1,1 +1,43 @@
+//Sounds zuweisen
+var sound = []; //Kann man hier auch const nutzen?
+sound[0] = new Audio(".assets/kick.mp3");
+sound[1] = new Audio(".assets/hihat.mp3");
+sound[2] = new Audio(".assets/snare.mp3");
+sound[3] = new Audio(".assets/A.mp3");
+sound[4] = new Audio(".assets/C.mp3");
+sound[5] = new Audio(".assets/F.mp3");
+sound[6] = new Audio(".assets/G.mp3");
+sound[7] = new Audio(".assets/laugh-1.mp3");
+sound[8] = new Audio(".assets/laugh-2.mp3");
+//Eventlistener
+document.querySelector("#pad1").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad2").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad3").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad4").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad5").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad6").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad7").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad8").addEventListener("click", function () {
+    playSample();
+});
+document.querySelector("#pad9").addEventListener("click", function () {
+    playSample();
+});
+//Abspiel Funktion
+function playSample() {
+}
 //# sourceMappingURL=typescript.js.map
